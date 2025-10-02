1dd3992332cb8eff479551c0b84cc7e2b3932c94
Mahdi <mbd7793@gmail.com>
2025-10-02 18:15:22 -0500

    trained model with CFG featrues

---
+++ scripts/run_train_baseline_log.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=gat_base_log
+#SBATCH --account=bdau-delta-gpu
+#SBATCH --partition=gpuH200x8-interactive
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=01:00:00
+#SBATCH --output=experiments/logs/train_baseline_log_%j.out
+#SBATCH --error=experiments/logs/train_baseline_log_%j.err
+
+cd /work/hdd/bdau/mbanisharifdehkordi/GAT_EFG
+PYTHON_PATH="$HOME/.conda/envs/gnn4_env/bin/python"
+
+srun $PYTHON_PATH scripts/train.py \
+    --config configs/model_config_baseline_log.yaml \
+    --experiment exp_006_baseline_log_reproduce
